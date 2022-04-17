b0147dcd8314f81c03198ddf4e0c7025da75c6a4
Lomovskoy <lomovskoy.kirill@yandex.ru>
2022-04-17 16:17:52 +0300

    #3 - Added an answer in the JSON format and pagination

---
+++ src/main/java/com/example/webflax/catalizator/domain/Message.java
@@ -0,0 +1,14 @@
+package com.example.webflax.catalizator.domain;
+
+public class Message {
+
+    private final String data;
+
+    public Message(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
--- src/main/java/com/example/webflax/catalizator/handlers/GreetingHandler.java
+++ src/main/java/com/example/webflax/catalizator/handlers/GreetingHandler.java
@@ -1,10 +1,12 @@
 package com.example.webflax.catalizator.handlers;
 
+import com.example.webflax.catalizator.domain.Message;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
@@ -15,13 +17,24 @@ public class GreetingHandler {
     private final static String USER = "user";
     private final static String GUEST = "Guest";
     private final static String INDEX_TEMP = "index";
-    private final static String HELLO_MESSAGE = "Hello, Spring";
 
     public Mono<ServerResponse> hello(ServerRequest serverRequest) {
-        var body = BodyInserters.fromValue(HELLO_MESSAGE);
+        var start = serverRequest.queryParam("start").map(Long::valueOf).orElse(0L);
+        var count = serverRequest.queryParam("count").map(Long::valueOf).orElse(3L);
+
+        var data = Flux.just(
+                "Hello reactive!",
+                "Mare then one",
+                "Third post",
+                "Fourth post",
+                "Fifth post")
+                .skip(start)
+                .take(count)
+                .map(Message::new);
+
         return ServerResponse.ok()
-                .contentType(MediaType.TEXT_PLAIN)
-                .body(body);
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(data, Message.class);
     }
 
     public Mono<ServerResponse> index(ServerRequest serverRequest) {
